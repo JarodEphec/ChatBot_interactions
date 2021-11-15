c2080f546486bb35b87895f84a8fb6524e88bb3d
Simon Kinet <64420845+Nepsylon@users.noreply.github.com>
2021-11-15 12:07:09 +0100

    Uptade MVP

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     def main():
         def get_ipv4(): #Fonction qui renvoie l'adresse ipv4
             ip4 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #Création du socket IPV4
-            ip4.connect(("8.8.8.8", 80))  # Connexion à l'adresse 8.8.8.8 de google e on regarde le port 80
+            ip4.connect(("8.8.8.8", 80))  # Connexion à l'adresse 8.8.8.8 de google et on regarde le port 80
             return ip4.getsockname()[0]
         def get_ipv6(): #Fonction qui renvoie l'adresse ipv6
             ip6 = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM) #Création du socket IPV6
@@ -20,22 +20,22 @@ if __name__ == "__main__":
             proc = subprocess.Popen('ipconfig', stdout=subprocess.PIPE) #Fonction qui exécuter ipconfig et crée un pipe
             while True: #on parcourt le pipe
                 line = proc.stdout.readline()
-                if ip.encode() in line: #On retrouve la ligne avec l'adresse ipv4 local
+                if ip.encode() in line: #On retrouve la ligne avec l'adresse ipv4 local récupérée avant
                     break
             netmask = proc.stdout.readline().rstrip().split(b':')[-1].replace(b' ', b'').decode() #On retire les caractères et extrait le masque de sous-réseau
             return netmask
 
         #Affichage des messages de bienvenu
         print('Bienvenu dans le Chatbot')
-        print('Vous pouvez tapez des mots-clés pour avori accès à vos données réseaux')
+        print('Vous pouvez tapez des mots-clés pour avoir accès à vos données réseaux')
         print('Pour mettre fin à la discussion tapez : fin')
         print('')
 
         flag = False #On définit un flag pour aider à boucler
-        choix = str(input('Choisissez un mot-clé :')).lower() #Input pour entrer les mots-clés
+
 
         while flag == False:
-            choix = str(input('Choissisez un autre mot-clé :')).lower()
+            choix = str(input('Choissisez un mot-clé :')).lower()
             if choix == 'ipv4':
                 print('IPV4 ADDRESS LOCAL :',get_ipv4() ) #Affichage de l'adresse IPV4
 
@@ -47,4 +47,6 @@ if __name__ == "__main__":
             elif choix == 'fin': #On passe le flag à true ça finit la boucle si l'utilisateur tape 'fin'
                 print('Merci d\'avoir utiliser le Chabot')
                 flag = True
+            else :
+                print('Choissisez une des trois commandes suivantes : ipv4 / ipv6 / masque')
 main()
