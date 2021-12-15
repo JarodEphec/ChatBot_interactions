1ff76774546acf99a861d636d6ff6aadbbdace24
RaphaelMarto <92374387+RaphaelMarto@users.noreply.github.com>
2021-12-15 13:15:54 +0100

    Class RoleManagement Completer + correction/ajout mineur

--- main.py
+++ main.py
@@ -5,8 +5,13 @@ from pymongo import MongoClient
 from src.config import config
 
 # tableau avec les mots-clés
-mots = ['up', 'stat', 'add', 'del', 'addto', 'shrole', 'shperm', 'help']
-desc = ['exemple', 'exemple', 'exemple', 'exemple', 'exemple', 'exemple', 'exemple', 'exemple']
+mots = ['up', 'stat', 'add', 'del', 'addTo', 'shRole', 'shPerm', "ShUtiRole", 'help']
+desc = ['affiche le statut du réseau', 'affiche le nombre d’utilisateurs total',
+        'ajouter un rôle à un utilisateur (Ex:add role utilisateur)', ' retirer un rôle à un utilisateur '
+                                                                      '(Ex:del role utilisateur)',
+        'ajouter un rôle à tous les noms utilisateurs (Ex:addTo role utilisateur1 utilisateur2)',
+        'affiche une liste des différents rôles', 'affiche les permissions d\'un role (Ex:shPerm role)',
+        'affiche une liste des utilisateurs par rôle (Ex: ShUtiRole role)', 'affiche toutes les commandes']
 
 
 def main():
@@ -32,6 +37,7 @@ def main():
     class NetworkStatCommand:
         @staticmethod
         def get_masque(self):  # Fonction qui renvoie le masque de sous-réseau de l'adresse ipv4
+            ip = ''
             network = NetworkStatCommand()
             if self == 'local':
                 ip = network.get_ipv4_local()
@@ -46,7 +52,8 @@ def main():
                 if ip.encode() in line:  # On retrouve la ligne avec l'adresse ipv4 local récupérée avant
                     break
             netmask = proc.stdout.readline().rstrip().split(b':')[-1].replace(b' ',
-                                                                              b'').decode()  # On retire les caractères et extrait le masque de sous-réseau
+                                                                              b'').decode()
+            # On retire les caractères et extrait le masque de sous-réseau
             return netmask
 
         def get_ipv4_local(self):  # Fonction qui renvoie l'adresse ipv4 local
@@ -96,13 +103,30 @@ def main():
             self.__role = role
             self.__user = user
 
+        @property
+        def role(self):
+            return self.__role
+
+        @role.setter
+        def role(self, value):
+            self.__role = value
+
+        @property
+        def user(self):
+            return self.__user
+
+        @user.setter
+        def user(self, value):
+            self.__user = value
+
         def add(self):
             try:
                 with DataBaseCommand() as connector:
                     collection = connector.db["users"]
                     list_users = collection.find()
                     for users in list_users:
-                        if self.__user == users['user_name'] and self.__role or int(self.__role) not in users['list_role']:
+                        if self.__user == users['user_name'] and (self.__role or int(self.__role) not in
+                                                                  users['list_role']):
                             new_value = {"$push": {"list_role": int(self.__role)}}
                             connector.db["users"].update_one({'user_name': self.__user}, new_value)
             except Exception as e:
@@ -114,10 +138,9 @@ def main():
                     collection = connector.db["users"]
                     list_users = collection.find()
                     for users in list_users:
-                        if self.__user == users['user_name'] and int(self.__role) or self.__role in users['list_role']:
-                            print("here")
-                            users['list_role'].remove(self.__role)
-                            print(users['list_role'])
+                        if self.__user == users['user_name'] and (int(self.__role) or self.__role in
+                                                                  users['list_role']):
+                            users['list_role'].remove(int(self.__role))
                             del_value = {"$set": {"list_role": users['list_role']}}
                             connector.db["users"].update_one({'user_name': self.__user}, del_value)
             except Exception as e:
@@ -129,7 +152,10 @@ def main():
                     collection = connector.db["users"]
                     list_users = collection.find()
                     for users in list_users:
-                        print("user:", users)
+                        if users['user_name'] in self.__user:
+                            if self.__role or int(self.__role) not in users['list_role']:
+                                new_value = {"$push": {"list_role": int(self.__role)}}
+                                connector.db["users"].update_one({'user_name': users['user_name']}, new_value)
             except Exception as e:
                 print(e)
 
@@ -154,16 +180,30 @@ def main():
                         if role['name'] == self.__role:
                             print("Permission:", role['perm_list'])
                     for perm in list_perm:
-                       print("Permission:", perm['name'])
+                        print("Permission:", perm['name'])
             except Exception as e:
                 print(e)
 
-        def help(self):
-            print('Liste des Commandes:')
-            i = 0
-            for cmd in mots:
-                print(' ', cmd, '-', desc[i])
-                i += 1
+        def show_user_role(self):
+            try:
+                with DataBaseCommand() as connector:
+                    collection = connector.db["users"]
+                    user_list = []
+                    list_user = collection.find()
+                    for user in list_user:
+                        if int(self.__role) in user['list_role']:
+                            user_list.append(user['user_name'])
+                    print('utilisateur ayant le role', self.__role, ":", end=' ')
+                    print(', '.join(user_list))
+            except Exception as e:
+                print(e)
+
+    def help():
+        print('Liste des Commandes:')
+        i = 0
+        for cmd in mots:
+            print(' ', cmd, '-', desc[i])
+            i += 1
 
     class Result:
 
@@ -174,6 +214,7 @@ def main():
             space = Result()
             local = LocalMachineCommand()
             network = NetworkStatCommand()
+            role_cmd = RoleManagementCommand()
             # Affichage des messages de bienvenu
             print('Bienvenu dans le Chatbot')
             space.get_space()
@@ -188,49 +229,72 @@ def main():
                 com.get_name()
                 choice = com.name
                 space.get_space()
-                if choice == 'up':
-                    print('LOCAL ADDRESS :')
-                    print('IPV4 ADDRESS :', local.get_data(network.get_ipv4_local()), '/',
-                          local.get_data(network.get_masque('local')))  # Affichage de l'adresse IPV4 local
-                    print('IPV6 ADDRESS :',
-                          local.get_data(network.get_ipv6_local()))  # Affichage de l'adresse IPV6 local
-                    space.get_space()
-                    print('PUBLIC ADDRESS :')
-                    print('IPV4 ADDRESS :', local.get_data(network.get_ipv4_public()), '/',
-                          local.get_data(network.get_masque('public')))  # Affichage de l'adresse IPV4 public
-                    space.get_space()
-                    print('PING :')
-                    local.get_data(network.get_ping())
-                    space.get_space()
-                elif choice == 'stat':
-                    count = 0
-                    try:
-                        with DataBaseCommand() as connector:
-                            collection = connector.db["users"]
-                            col = collection.find()
-                            for i in col:
-                                count += 1
-                            print('Il y a ' + str(count) + ' utilisateur(s).')
+                try:
+                    if choice == 'up':
+                        print('LOCAL ADDRESS :')
+                        print('IPV4 ADDRESS :', local.get_data(network.get_ipv4_local()), '/',
+                              local.get_data(network.get_masque('local')))  # Affichage de l'adresse IPV4 local
+                        print('IPV6 ADDRESS :',
+                              local.get_data(network.get_ipv6_local()))  # Affichage de l'adresse IPV6 local
                         space.get_space()
-                    except Exception as e:
-                        print(e)
-                elif choice == 'add':
-                    RoleManagementCommand(com.param[0], com.param[1]).add()
-                elif choice == 'del':
-                    RoleManagementCommand(com.param[0], com.param[1]).dell()
-                elif choice == 'addto':
-                    RoleManagementCommand().add_to()
-                elif choice == 'shrole':
-                    RoleManagementCommand().show_role()
-                elif choice == 'shperm':
-                    RoleManagementCommand(com.param[0]).show_perm()
-                elif choice == 'help':
-                    RoleManagementCommand().help()
-                elif choice == 'fin':  # On passe le flag à true ça finit la boucle si l'utilisateur tape 'fin'
-                    print('Merci d\'avoir utilisé le Chabot')
-                    flag = True
-                else:
-                    print('Choissisez une des commandes suivantes : up, stat, add, shRole, shPerm')
+                        print('PUBLIC ADDRESS :')
+                        print('IPV4 ADDRESS :', local.get_data(network.get_ipv4_public()), '/',
+                              local.get_data(network.get_masque('public')))  # Affichage de l'adresse IPV4 public
+                        space.get_space()
+                        print('PING :')
+                        local.get_data(network.get_ping())
+                        space.get_space()
+                    elif choice == 'stat':
+                        count = 0
+                        try:
+                            with DataBaseCommand() as connector:
+                                collection = connector.db["users"]
+                                col = collection.find()
+                                for i in col:
+                                    count += 1
+                                print('Il y a ' + str(count) + ' utilisateur(s).')
+                            space.get_space()
+                        except Exception as e:
+                            print(e)
+                    elif choice == 'add':
+                        role_cmd.role = com.param[0]
+                        role_cmd.user = com.param[1]
+                        role_cmd.add()
+                    elif choice == 'del':
+                        role_cmd.role = com.param[0]
+                        role_cmd.user = com.param[1]
+                        role_cmd.dell()
+                    elif choice == 'addto':
+                        role_cmd.role = com.param[0]
+                        role_cmd.user = com.param[1:]
+                        role_cmd.add_to()
+                    elif choice == 'shrole':
+                        role_cmd.show_role()
+                    elif choice == 'shperm':
+                        role_cmd.role = com.param[0]
+                        role_cmd.show_perm()
+                    elif choice == 'shutirole':
+                        role_cmd.role = com.param[0]
+                        role_cmd.show_user_role()
+                    elif choice == 'help':
+                        help()
+                    elif choice == 'check':
+                        try:
+                            with DataBaseCommand() as connector:
+                                collection = connector.db["users"]
+                                list_users = collection.find()
+                                for users in list_users:
+                                    print("user:", users)
+                        except Exception as e:
+                            print(e)
+                    elif choice == 'fin':  # On passe le flag à true ça finit la boucle si l'utilisateur tape 'fin'
+                        print('Merci d\'avoir utilisé le Chabot')
+                        flag = True
+                    else:
+                        print('Choissisez une des commandes suivantes : up, stat, add, addTo, del, shRole, shPerm, '
+                              'ShUtiRole, Help')
+                except IndexError:
+                    print('Veuillez entrée les arguments nécessaire')
 
     res = Result()
     res.display_result()
